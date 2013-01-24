fafc239c0d98d88904c479ffdbb8ce88e9436c1a
oga <8monkey.theorem@gmail.com>
2013-01-25 02:21:39 +0900

    use jaccard similarity.

--- prototype/aoj_recommender.py
+++ prototype/aoj_recommender.py
@@ -4,12 +4,10 @@ import sys
 import time
 import math
 
-def cos(v1, v2):
-    numerator = sum([v1[c] * v2[c] for c in v1 if c in v2])
-    square = lambda x: x * x
-    denominator =  math.sqrt(sum(map(square, v1.values())) * sum(map(square, v2.values())))
+def jaccard(v1, v2):
+    numerator = sum([c in v2 for c in v1])
+    denominator = len(v1) + len(v2) - numerator
     return float(numerator) / denominator if denominator != 0 else 0
-
 
 user = {}
 
@@ -20,16 +18,16 @@ for line in sys.stdin:
     if count == 2:
         id = line
     if count == 3:
-        problem = dict(map(lambda x: (x, 1), line.split()))
+        problem = set(line.split())
         user[id] = problem
         count = 0
 
 for u1, p1 in user.iteritems():
     temp = []
     for u2, p2 in user.iteritems():
-        if u1 == u2:
+        if p1.issubset(p2) or p1.issuperset(p2):
             continue
-        sim = cos(p1, p2)
+        sim = jaccard(p1, p2)
         if sim != 1.0:
             temp.append((sim, u1, u2))
     temp.sort()
